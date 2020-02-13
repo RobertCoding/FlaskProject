91098bd4a85d60c4b9049b497f97cdfefa04ab2f
RobertCoding <rabbitmaize@gmail.com>
2020-02-13 23:37:38 +0800

    render_template and index.html

--- py_flask.py
+++ py_flask.py
@@ -4,13 +4,13 @@ Learn Flask for Python - Full Tutorial
 Day 1
 '''
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
